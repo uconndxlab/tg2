e67496305afa98d4e90b447ae6676b408ba8d10c
joel <joel@DM-DX-001.grove.ad.uconn.edu>
2023-02-21 15:52:01 -0500

    oh boy...

--- public/index.php
+++ public/index.php
@@ -18,27 +18,48 @@ $twig = Twig::create('templates', ['cache' => false]);
 // Add Twig-View Middleware
 $app->add(TwigMiddleware::create($app, $twig));
 
+/**  Begin Main Routes */
 $app->get('/', function ($request, $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'home.html');
 })->setName('home');
 
-// dashboard route
+
 $app->get('/{part}', function ($request, $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'home.html', ['part' => $args['part']]);
-})->setName('dashboard');
+});
 
+/** End Main Routes */
 
-$app->get('/parts/dashboard', function ($request, $response, $args) {
+/** Begin Fragments */
+
+/** Dashboard Part -- my reports */
+$app->get('/parts/my-reports', function ($request, $response, $args) {
     $view = Twig::fromRequest($request);
-    return $view->render($response, 'parts/dashboard.html');
+    return $view->render($response, 'parts/my-reports.html');
 })->setName('parts/dashboard');
 
+/** Part -- my profile */
 $app->get('/parts/profile', function ($request, $response, $args) {
     $view = Twig::fromRequest($request);
     return $view->render($response, 'parts/profile.html');
 })->setName('parts/profile');
+
+/** Part -- new report */
+$app->get('/parts/new-report', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'parts/new-report.html');
+})->setName('parts/new-report');
+
+
+/** End Fragments */
+
+/** Begin API Routes */
+
+
+
+/** End API Routes */
 
 
 $app->run();
